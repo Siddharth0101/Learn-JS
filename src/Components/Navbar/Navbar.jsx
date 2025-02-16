e4dedfbde0c5ffe0bcb466c1aa5d3a42b3026ac6
Siddharth0101 <147489528+Siddharth0101@users.noreply.github.com>
2025-02-16 16:52:37 +0530

    added contionally color for navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,5 +1,3 @@
-"use client"
-
 import { useDispatch, useSelector } from "react-redux";
 import ThemeToggle from "./ThemeToggle";
 import { ThemeActions } from "@/Store/ThemeSlice";
@@ -7,14 +5,13 @@ import { ThemeActions } from "@/Store/ThemeSlice";
 export default function Navbar() {
     const themeSelect=useSelector((state)=>state.Theme.theme)
     const dispatch=useDispatch()
-    console.log(themeSelect);
     
     const toggleTheme=()=>{
-        dispatch(ThemeActions.ChangeTheme(themeSelect === "DARK" ? "RETRO" : "DARK"));
+        dispatch(ThemeActions.ChangeTheme(themeSelect === "DARK" ? "LIGHT" : "DARK"));
     }
 
     return <nav>
-        <div className="w-full bg-indigo-900 p-8">
+        <div className={`w-full ${themeSelect=="LIGHT"?"bg-gray-600":"bg-gray-900"} p-3`}>
             <ThemeToggle currentTheme={themeSelect} onClick={toggleTheme}/>
         </div>
     </nav>
--- src/Components/Navbar/ThemeToggle.jsx
+++ src/Components/Navbar/ThemeToggle.jsx
@@ -1,18 +1,18 @@
 export default function ThemeToggle({ currentTheme, onClick }) {
   return (
-    <div className="flex justify-end">
+    <div className="flex justify-center">
       <div
-        className={`relative w-24 h-10 flex items-center p-1 rounded-full cursor-pointer transition-all duration-500 shadow-md ${
-          currentTheme === "RETRO" ? "bg-indigo-400" : "bg-gray-800"
-        }`}
+        className={`relative w-24 h-10 flex items-center p-1 rounded-full cursor-pointer transition-all duration-500 border-2 ${
+          currentTheme === "LIGHT" ? "bg-gray-900" : "bg-gray-500 border-gray-600"
+        }` }
         onClick={onClick}
       >
         <span className="text-2xl">🌙</span>
-        <span className="text-2xl ml-auto">🎸</span>
+        <span className="text-2xl ml-auto">☀️</span>
         <div
-          className={`absolute w-8 h-9 rounded-full flex items-center justify-center text-3xl transition-all duration-500 shadow-sm ${
-            currentTheme === "RETRO"
-              ? "translate-x-14 rotate-180 bg-orange-400 border-2 border-yellow-300"
+          className={`absolute w-7 h-9 rounded-full flex items-center justify-center transition-all duration-500 ${
+            currentTheme === "LIGHT"
+              ? "translate-x-14 rotate-180 bg-gray-900 border-2"
               : "translate-x-0 bg-gray-600 border-2 border-gray-400"
           }`}
         ></div>
