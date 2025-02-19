5399c0d804cbd02435e432649ff05342eca48707
Siddharth0101 <147489528+Siddharth0101@users.noreply.github.com>
2025-02-19 22:42:00 +0530

    alert message

--- src/Components/Alert/Alert.jsx
+++ src/Components/Alert/Alert.jsx
@@ -1,8 +1,8 @@
 export default function Alert({ currentTheme, icon, message,onclose }) {
     return (
-      <div className={`w-2/12 h-12 ${currentTheme === "LIGHT" ? "bg-gray-600" : "bg-gray-950"} rounded-lg flex items-center p-3 cursor-pointer`} onClick={onclose}>
-        <span className="mr-2">{icon}</span>
-        <span className={`${currentTheme === "LIGHT" ? "text-gray-100" : "text-gray-400"}`}>{message}</span>
+      <div className={`fixed bottom-5 right-5 max-w-sm ${currentTheme === "LIGHT" ? "bg-gray-600" : "bg-gray-950"} rounded-lg flex items-center p-3 cursor-pointer`} onClick={onclose}>
+        <span className="p-5 text-6xl">{icon}</span>
+        <span className={`${currentTheme === "LIGHT" ? "text-gray-100" : "text-gray-400"} text-xl`}>{message}</span>
       </div>
     );
   }
