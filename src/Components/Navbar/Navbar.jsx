5c7356a1fd0e168be687cbf0ce6c8a907b17004f
Siddharth0101 <147489528+Siddharth0101@users.noreply.github.com>
2025-02-19 21:20:42 +0530

    alert card

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,20 +1,32 @@
 import { useDispatch, useSelector } from "react-redux";
 import ThemeToggle from "./ThemeToggle";
 import { ThemeActions } from "@/Store/ThemeSlice";
+import Alert from "../Alert/Alert";
+import { useEffect, useState } from "react";
 
 export default function Navbar() {
+
+    const [showAlert,setShowAlert]=useState(false)
+
     const themeSelect = useSelector((state) => state.Theme.theme);
     const dispatch = useDispatch();
     
     const toggleTheme = () => {
         localStorage.setItem("theme", themeSelect === "DARK" ? "LIGHT" : "DARK");
         dispatch(ThemeActions.ChangeTheme(themeSelect === "DARK" ? "LIGHT" : "DARK"));
+        setShowAlert(true)
     };
+
+    useEffect(()=>{
+        const timer = setTimeout(() => setShowAlert(false), 3000);
+        return () => clearTimeout(timer);
+    },[showAlert])
 
     return (
         <nav>
-            <div className={`w-full ${themeSelect === "LIGHT" ? "bg-gray-600" : "bg-gray-900"} p-3`}>
+            <div className={`w-full ${themeSelect === "LIGHT" ? "bg-gray-500" : "bg-gray-800"} p-3`}>
                 <ThemeToggle currentTheme={themeSelect} onClick={toggleTheme} />
+              {showAlert &&   <Alert currentTheme={themeSelect} onclose={()=>setShowAlert(false)}/>}
             </div>
         </nav>
     );
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,27 @@
 "use client";
 
-import { Provider } from "react-redux";
+import { Provider, useSelector } from "react-redux";
+import { useState, useEffect } from "react";
 import "./globals.css";
 import ReduxStore from "@/Store/ReduxStore";
 import Navbar from "@/Components/Navbar/Navbar";
+
 export default function RootLayout({ children }) {
   return (
+    <Provider store={ReduxStore}>
+      <RootLayoutContent>{children}</RootLayoutContent>
+    </Provider>
+  );
+}
+
+function RootLayoutContent({ children }) {
+  const themeSelect = useSelector((state) => state.Theme.theme);
+
+  return (
     <html lang="en">
-      <body>
-      <Provider store={ReduxStore}>
-        <Navbar/>
+      <body className={`${themeSelect === "LIGHT" ? "bg-gray-400" : "bg-gray-600"} min-h-screen`}>
+        <Navbar />
         {children}
-      </Provider>
       </body>
     </html>
   );
